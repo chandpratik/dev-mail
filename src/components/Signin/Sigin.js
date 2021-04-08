517e443df306ce516ed1a28bc02f43b6bb63c71b
pratikal1 <chandpratikal1@gmail.com>
2021-04-09 01:32:08 +0530

    added basic SigIn component

--- src/components/Signin/Sigin.js
+++ src/components/Signin/Sigin.js
@@ -1,9 +1,36 @@
+import { TextField } from '@material-ui/core';
 import React from 'react';
-
+import './styles.css';
 const Sigin = () => {
   return (
-    <div>
-      <h1>Sign In Page</h1>
+    <div className="login">
+      <div className="login__content">
+        <div className="login__loading">
+          <div className="login__wrapper">
+            <img
+              className="login__logo"
+              src="/assets/svg/001-paper-plane.svg"
+              alt=""
+            />
+            <p className="login__title">Sign In </p>
+            <p className="login__subtitle">Continue to DevMail </p>
+            <form className="login__form">
+              <TextField
+                id="outlined-basic"
+                label="Email"
+                variant="outlined"
+                type="email"
+              />
+              <TextField
+                id="outlined-basic"
+                label="Email"
+                variant="outlined"
+                type="password"
+              />
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
