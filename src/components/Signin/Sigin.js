6f9216c7b2089e199859e1a58f6c369326f075ff
pratikal1 <chandpratikal1@gmail.com>
2021-04-12 14:47:25 +0530

    made changes & added styles to sign in page

--- src/components/Signin/Sigin.js
+++ src/components/Signin/Sigin.js
@@ -1,36 +1,72 @@
-import { TextField } from '@material-ui/core';
-import React from 'react';
+import { TextField, Button } from '@material-ui/core';
+import React, { useState } from 'react';
+import Signup from '../Signup/Signup';
 import './styles.css';
 const Sigin = () => {
+  const [showSignup, setshowSignup] = useState(false);
+
+  const toggleSignUp = (e) => {
+    e.preventDefault();
+    setshowSignup(true);
+  };
+
   return (
     <div className="login">
-      <div className="login__content">
-        <div className="login__loading">
-          <div className="login__wrapper">
-            <img
-              className="login__logo"
-              src="/assets/svg/001-paper-plane.svg"
-              alt=""
-            />
-            <p className="login__title">Sign In </p>
-            <p className="login__subtitle">Continue to DevMail </p>
-            <form className="login__form">
-              <TextField
-                id="outlined-basic"
-                label="Email"
-                variant="outlined"
-                type="email"
+      {showSignup ? (
+        <Signup showSignup={showSignup} setshowSignup={setshowSignup} />
+      ) : (
+        <div className="login__content">
+          <div className="login__loading">
+            <div className="login__wrapper">
+              <img
+                className="login__logo"
+                src="/assets/svg/001-paper-plane.svg"
+                alt="logo"
               />
-              <TextField
-                id="outlined-basic"
-                label="Email"
-                variant="outlined"
-                type="password"
-              />
-            </form>
+              <p className="login__title">Sign In </p>
+              <p className="login__subtitle">Continue to DevMail </p>
+              <form className="login__form">
+                <TextField
+                  id="outlined-basic"
+                  label="Email"
+                  variant="outlined"
+                  className="login__input"
+                  type="email"
+                />
+                <TextField
+                  id="outlined-basic"
+                  label="Password"
+                  variant="outlined"
+                  className="login__input"
+                  type="password"
+                />
+
+                <div className="login__infotext">
+                  Not your computer ? Use guest mode to sign in privately?
+                  <a href="/learnmore">Learn More</a>
+                </div>
+
+                <div className="login__buttons">
+                  <Button
+                    className="login__button"
+                    color="primary"
+                    onClick={(e) => toggleSignUp(e)}
+                  >
+                    Create Account
+                  </Button>
+                  <Button
+                    className="lgoin__button"
+                    color="primary"
+                    variant="contained"
+                  >
+                    Sign in
+                  </Button>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
