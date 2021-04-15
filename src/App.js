781a7843f4e9a67485e493725565e6628a69e0d7
pratikal1 <chandpratikal1@gmail.com>
2021-04-15 20:22:41 +0530

    removed inactive lines

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { useEffect } from 'react';
 import { HomePage, Loading, Signin } from './components';
 import { useLocalContext } from './context/context';
-import { useMailContext } from './context/MailContext';
+
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   const { appState, setAppState } = useLocalContext();
-  const { onScreenMails } = useMailContext();
 
   useEffect(() => {
     if (appState === 'loading') {
